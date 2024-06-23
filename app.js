f750d798478573693c7f1e78c1f9627813ebdd9e
liansun96 <toetetaung3296@gmail.com>
2024-06-23 23:48:12 +0630

    setup server

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+const start = async() => {
+    try {
+        app.listen(port , console.log(`Server is listening on ${port}...`))
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
